0d11d88e4001bfd1433ea9be1d04d08a24309cca
Namita Anand <you@example.com>
2025-08-31 23:06:06 -0500

    commit problem 1 code

---
+++ Module 1 group assignment.R
@@ -0,0 +1,22 @@
+install.packages("ISLR")
+library(ISLR)
+dim (College)
+help(College)
+apps<-College$Apps
+head(College)
+
+#Problem 1
+
+# running a bootstrap confidence interval for the apps variable using 1000 bootstrap samples
+install.packages ("dplyr")
+library (dplyr)
+set.seed (1900)
+boots <- NULL
+for(i in 1:10000) {
+  meanApps <- apps%>%sample(size = 1000,replace=TRUE)%>%mean()
+  boots <- c(boots,meanApps)
+}
+boots%>%hist(main="Sample Distribution for Mean Apps")
+
+# now creating the intervals for 80% confidence interval
+quantile(boots, c(.1,.9))
