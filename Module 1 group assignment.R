ddcf41130ea5012c8e28f5bb65daa1bda2ad581d
Namita Anand <you@example.com>
2025-09-01 20:16:06 -0500

    adding problem 2 code

--- Module 1 group assignment.R
+++ Module 1 group assignment.R
@@ -12,11 +12,25 @@ install.packages ("dplyr")
 library (dplyr)
 set.seed (1900)
 boots <- NULL
-for(i in 1:10000) {
+for(i in 1:1000) {
   meanApps <- apps%>%sample(size = 1000,replace=TRUE)%>%mean()
   boots <- c(boots,meanApps)
 }
 boots%>%hist(main="Sample Distribution for Mean Apps")
 
 # now creating the intervals for 80% confidence interval
+quantile(boots, c(.1,.9))
+
+#Problem 2
+
+#random seed 1
+set.seed (2016)
+boots <- NULL
+for (i in 1:1000) {
+  meanApps <-apps%>%sample(size = 1000,replace=TRUE)%>%mean()
+  boots <- c(boots,meanApps)
+}
+boots%>%hist(main="Sample Distribution for Mean Apps")
+
+# now creating the intervals for 80% confidence interval for seed 1
 quantile(boots, c(.1,.9))
